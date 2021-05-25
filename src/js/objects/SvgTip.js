a9c9d49eb2070296bb61849804a2cf62102902e8
milospetrovic <milosp@ultimatemovers.net>
2021-05-25 15:41:26 +0200

    test

--- src/js/objects/SvgTip.js
+++ src/js/objects/SvgTip.js
@@ -64,7 +64,7 @@ export default class SvgTip {
 
 		this.listValues.map((set, i) => {
 			const color = this.colors[i] || 'black';
-			let value = set.formatted.v === 0 || set.formatted.v ? set.formatted.v : set.value;
+			let value = 'Hello World';//set.formatted.v === 0 || set.formatted.v ? set.formatted.v : set.value;
 
 			let li = $.create('li', {
 				styles: {
